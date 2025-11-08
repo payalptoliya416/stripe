5abba6f4a022e7fa236f5e5cacc2c2b7380922a4
payalptoliya416 <payal.rising416@gmail.com>
2025-11-08 09:38:41 +0530

    code

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,28 +1,16 @@
 
-document.querySelectorAll(".option-list").forEach(list => {
-  const options = list.querySelectorAll("li");
-  let answered = false; 
+const options = document.querySelectorAll(".option-list li");
 
-  options.forEach(o => {
-    if (o.classList.contains("correct") || o.classList.contains("wrong")) {
-      answered = true; 
-      o.querySelector("input[type='radio']").checked = true; 
-    }
-  });
-
-  options.forEach(option => {
-    option.addEventListener("click", () => {
-      if (answered) return;
-
-      options.forEach(o => {
-        o.classList.remove("active");
-        o.querySelector("input[type='radio']").checked = false;
-      });
-
-      option.classList.add("active");
-      option.querySelector("input[type='radio']").checked = true;
-
-      answered = true;
+options.forEach(option => {
+  option.addEventListener("click", () => {
+    // Remove active class from all
+    options.forEach(o => {
+      o.classList.remove("active");
+      o.querySelector("input[type='radio']").checked = false;
     });
+
+    // Add active class to clicked option
+    option.classList.add("active");
+    option.querySelector("input[type='radio']").checked = true;
   });
 });
